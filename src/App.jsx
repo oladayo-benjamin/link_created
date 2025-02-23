a93e5d8be2bde2771a45ed391703bb343664679b
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-02-23 22:38:18 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/components/transactionConfirmationUI/Payment.jsx 	new file:   src/components/transactionConfirmationUI/PaymentLinkPage.jsx 	new file:   src/components/transactionConfirmationUI/PaymentSuccessMessage.jsx 	new file:   src/components/transactionConfirmationUI/makePaymentAssets/check.png 	new file:   src/components/transactionConfirmationUI/makePaymentAssets/flutterwave_logo.png 	new file:   src/components/transactionConfirmationUI/makePaymentAssets/monnify_logo.png 	new file:   src/components/transactionConfirmationUI/makePaymentAssets/paypal_logo.png 	new file:   src/components/transactionConfirmationUI/makePaymentAssets/quickteller_logo.png 	new file:   src/components/transactionConfirmationUI/makePaymentPage.jsx 	new file:   src/components/transactionConfirmationUI/paymentLinkAssets/check.png 	new file:   src/components/transactionConfirmationUI/paymentLinkAssets/email.png 	new file:   src/components/transactionConfirmationUI/paymentLinkAssets/sms.png 	new file:   src/components/transactionConfirmationUI/paymentLinkAssets/whatsapp.png 	new file:   src/components/transactionConfirmationUI/successMessageAssets/success.jpg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Payment from "./components/transactionConfirmationUI/Payment";
-
+import PaymentSuccessMessage from "./components/transactionConfirmationUI/PaymentSuccessMessage";
+import PaymentLinkPage from "./components/transactionConfirmationUI/PaymentLinkPage";
+import MakePaymentPage from "./components/transactionConfirmationUI/makePaymentPage";
 
 
 function App() {
   return (
     <ChakraProvider>
       <Payment />
+      <PaymentSuccessMessage />
+      <PaymentLinkPage />
+      <MakePaymentPage />
     </ChakraProvider>
   );
 }
